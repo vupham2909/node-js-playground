3c57b3195f0e833a8b6756d166881c9cf6fd808d
Vu Pham <phamquangvu2909@hotmail.com>
2019-02-01 07:38:29 +0700

    Update put, patch,delete to book router

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
 const debug = require('debug')('app');
 
@@ -9,8 +10,8 @@ function router(Book) {
     .get((req, res) => {
       const { query } = req;
       Book.find(query, (err, books) => {
-        if (err) res.status(500).send(err);
-        res.json(books);
+        if (err) return res.status(500).send(err);
+        return res.json(books);
       });
     })
     .post((req, res) => {
@@ -20,14 +21,55 @@ function router(Book) {
 
       res.status(201).json(book);
     });
-
-  bookRouter.route('/books/:id').get((req, res) => {
+  bookRouter.use('/books/:id', (req, res, next) => {
     const { id } = req.params;
     Book.findById(id, (err, book) => {
-      if (err) res.status(500).send(err);
-      res.json(book);
+      if (err) return res.status(500).send(err);
+      if (!book) return res.sendStatus(404);
+
+      req.book = book;
+      return next();
     });
   });
+  bookRouter
+    .route('/books/:id')
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.genre = req.body.genre;
+      book.author = req.body.author;
+
+      book.save((err) => {
+        if (err) return res.status(500).send(err);
+        return res.json(book);
+      });
+
+      return res.send(book);
+    })
+    .patch((req, res) => {
+      const { book, body } = req;
+
+      // eslint-disable-next-line no-underscore-dangle
+      if (body._id) delete body._id;
+
+      Object.entries(body).forEach((element) => {
+        const key = element[0];
+        const value = element[1];
+        book[key] = value;
+      });
+
+      book.save((err) => {
+        if (err) return res.status(500).send(err);
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) return res.status(500).send(err);
+        return res.sendStatus(204);
+      });
+    });
 
   return bookRouter;
 }
